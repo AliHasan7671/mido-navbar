d27a134f26c3be9afd8e8326756f927100a08f89
Ali Hasan <admin@ialihasan.com>
2018-09-05 15:37:26 +0500

    Disable backlight of capacitive buttons.

---
+++ common/service.sh
@@ -0,0 +1,9 @@
+#!/system/bin/sh
+# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
+# This will make your scripts compatible even if Magisk change its mount point in the future
+MODDIR=${0%/*}
+echo '0' > /sys/devices/soc/leds-qpnp-9/leds/button-backlight/max_brightness
+echo '0' > /sys/devices/soc/leds-qpnp-9/leds/button-backlight1/brightness
+
+# This script will be executed in late_start service mode
+# More info in the main Magisk thread
