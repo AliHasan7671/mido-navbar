ae58abf1ba58ac10a96ad9f03194890e18bb5bb4
Ali Hasan <admin@ialihasan.com>
2018-09-07 19:25:26 +0500

    Disable button brightness: Typo

--- common/service.sh
+++ common/service.sh
@@ -3,7 +3,7 @@
 # This will make your scripts compatible even if Magisk change its mount point in the future
 MODDIR=${0%/*}
 echo '0' > /sys/devices/soc/leds-qpnp-9/leds/button-backlight/max_brightness
-echo '0' > /sys/devices/soc/leds-qpnp-9/leds/button-backlight1/brightness
+echo '0' > /sys/devices/soc/leds-qpnp-9/leds/button-backlight/brightness
 
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
