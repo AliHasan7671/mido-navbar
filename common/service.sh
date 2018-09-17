877902dcd5eeb0ae646d0b3cc9f7f3e7c3aa8ccf
Ali Hasan <admin@ialihasan.com>
2018-09-17 17:03:12 +0500

    Completely disable buttons instead of un-mapping.
    This will disable buttons completely instead of just un-mapping keys, making it to support all panels used in mido.

--- common/service.sh
+++ common/service.sh
@@ -2,8 +2,13 @@
 # Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
 # This will make your scripts compatible even if Magisk change its mount point in the future
 MODDIR=${0%/*}
+
+# Disable backlight
 echo '0' > /sys/devices/soc/leds-qpnp-9/leds/button-backlight/max_brightness
 echo '0' > /sys/devices/soc/leds-qpnp-9/leds/button-backlight/brightness
+
+# Disable buttons
+echo '1' > /proc/touchpanel/capacitive_keys_disable
 
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
